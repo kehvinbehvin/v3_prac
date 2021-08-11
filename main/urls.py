2719e11600ca975f63082c28ab684419c686f64c
Kevin Ho <hokevin78@gmail.com>
2021-08-11 23:11:41 +0800

    added a router

--- main/urls.py
+++ main/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from first_app.views import FirstViewSet
+
+# create a new router
+router = routers.DefaultRouter()
+# register our viewsets
+router.register(r'first', FirstViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
